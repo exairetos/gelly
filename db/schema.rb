3a8810994045ca597c8f0df50ca90a212fe8e039
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-06-05 20:42:37 +0300

    Drop filter manufacturers resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_175213) do
-
-  create_table "manufacturers", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_05_26_174540) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
