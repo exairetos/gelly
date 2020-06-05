1ffb84fe8b5edac9d6856a7079ef7cd53833ab96
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-06-05 21:51:44 +0300

    Build manufacturers resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_174540) do
+ActiveRecord::Schema.define(version: 2020_05_26_175213) do
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
